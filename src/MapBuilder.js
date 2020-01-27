abb60046fbb6045e4f5200be9b45764faef914e9
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2020-01-27 12:02:51 +0400

    [i18n][m]: adds i18n support (#3)
    * [i18n][s]: installs dependencies
    
    * [i18n][s]: adds translates
    
    * [tests][s]: updates 'renders UI' snapshot
    
    * [i18n][s]: updates translations after native-speaker review
    
    * [build][s]: runs build:package command

--- src/MapBuilder.js
+++ src/MapBuilder.js
@@ -1,14 +1,20 @@
+import "./i18n/i18n"
+
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import './css/tailwind.css';
+import {useTranslation} from "react-i18next"
 
 export const MapBuilder = (props) => {
   // Make a copy of passed view so that we don't mutate it:
   const view = JSON.parse(JSON.stringify(props.view))
+
+  const { t } = useTranslation();
+
   if (!view.resources) {
-    return (<div>MapBuilder requires resource to be compiled into view.</div>)
+    return (<div>{t('MapBuilder requires resource to be compiled into view.')}</div>)
   } else if (!view.resources[0] || !view.resources[0].schema) {
-    return (<div>MapBuilder requires resource schema.</div>)
+    return (<div>{t('MapBuilder requires resource schema.')}</div>)
   }
   // TODO: make it work with multiple resources
   const fields = view.resources[0].schema
@@ -44,7 +50,7 @@ export const MapBuilder = (props) => {
           <Form className="bg-white text-xs p-3 text-left ">
             <div className="flex flex-wrap ">
               <div className="w-full mb-3">
-                <label htmlFor="lonField" className="text-xs font-bold uppercase text-gray-700">Longitude field</label>
+                <label htmlFor="lonField" className="text-xs font-bold uppercase text-gray-700">{t('Longitude field')}</label>
                 <div className="relative">
                   <Field name="lonField" component="select" placeholder="Longitude field" className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                     {fields.map((field, index) => {
@@ -58,7 +64,7 @@ export const MapBuilder = (props) => {
               </div>
 
               <div className="w-full mb-3">
-                <label htmlFor="latField" className="text-xs  font-bold uppercase text-gray-700">Latitude field</label>
+                <label htmlFor="latField" className="text-xs  font-bold uppercase text-gray-700">{t('Latitude field')}</label>
                 <div className="relative">
                   <Field name="latField" component="select" placeholder="Latitude field" className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                     {fields.map((field, index) => {
@@ -72,16 +78,16 @@ export const MapBuilder = (props) => {
               </div>
 
             <div className="w-full mb-3">
-                <label htmlFor="infobox" className="text-xs  font-bold uppercase text-gray-700">Infobox</label>
+                <label htmlFor="infobox" className="text-xs  font-bold uppercase text-gray-700">{t('Infobox')}</label>
                 <div className="relative">
-                  <Field name="infobox" type="text" placeholder="My popup: ${data.fieldName}" className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                  <Field name="infobox" type="text" placeholder={t('My popup') + ": ${data.fieldName}"} className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
                 </div>
             </div>
 
             <div className="w-full mb-3">
               <div className="flex justify-center">
                   <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">
-                    Add map
+                    {t('Add map')}
                   </button>
               </div>
             </div>
